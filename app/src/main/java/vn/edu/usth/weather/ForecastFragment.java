52167e653499c25cc5ba85b0146e50167a9166fc
Paxton <dracarys1312@gmail.com>
2020-12-13 00:30:05 +0700

    Practical Work 3

---
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,26 @@
+package vn.edu.usth.weather;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        view.setBackgroundColor(Color.parseColor("#20FF0000"));
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        if (savedInstanceState == null) {
+            // During initial setup, plug in the details fragment.
+            // create fragment
+            Log.i("WeatherActivity","Create Fragment");
+            ForecastFragment forecastFragment = new ForecastFragment();
+
+            forecastFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, forecastFragment).commit();
+        }
+
         Log.i("WeatherAcitivity", "onCreate");
     }
 
