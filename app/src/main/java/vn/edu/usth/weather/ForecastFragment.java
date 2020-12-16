e2e27ad791d928e7e0499fc31a66bbf26f1f05d8
Paxton <dracarys1312@gmail.com>
2020-12-16 20:50:26 +0700

    First implementation of RecyclerView

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -4,13 +4,17 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
-
 public class ForecastFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private MyAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,26 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
         rootView.setBackgroundColor(Color.parseColor("#dedefd"));
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performasnce if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        // recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<String> values = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            values.add("Test" + i);
+        }
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(values);
+        recyclerView.setAdapter(mAdapter);
+
 
         return rootView;
     }
