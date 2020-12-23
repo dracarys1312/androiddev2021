3da971122a9a54ac8fb6d66e14c9859722357c83
Paxton <dracarys1312@gmail.com>
2020-12-24 01:47:08 +0700

    Practical Work 11

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -6,20 +6,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
+    private static final int BUFFER_SIZE = 1024;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,24 @@ public class WeatherActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        try {
+            AssetManager assetFiles = getAssets();
+            InputStream in = null;
+            OutputStream out = null;
+            in = assetFiles.open("samplesong.mp3");
+            out = new FileOutputStream(
+                    Environment.getExternalStorageDirectory() + "/samplesong.mp3");
+            copyAssetFiles(in, out);
+            Log.i("WeatherActivity", "copyAssetFiles");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.parse(Environment.getExternalStorageDirectory().getPath()+ "/samplesong.mp3"));
+        mediaPlayer.start();
     }
     private void addTabs(ViewPager viewPager){
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -61,6 +93,31 @@ public class WeatherActivity extends AppCompatActivity {
 
         public CharSequence getPageTitle(int position){
             return fragmentTitleList.get(position);
+        }
+    }
+
+    private static void copyAssetFiles(InputStream in, OutputStream out) {
+        try {
+
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int read;
+
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     @Override
